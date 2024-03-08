47a294fb18db77998417fcc83410ad83cf08aeb9
Sheeba Ali <Sheeba.Ali@novusaltair.com>
2024-03-08 09:36:07 +0500

    new updates

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import userRoute from './routes/user.js';
 
 const port = 4000;
 const app = express();
 
+app.use("/api/v1/user" , userRoute);
 app.listen(port, () => {
     console.log(`express started at http://localhost:${port}`);
 })
---
+++ src/controllers/user.ts
@@ -0,0 +1,7 @@
+import { Request,Response, NextFunction } from "express";
+
+
+
+export const newUser = (req:Request, res:Response, next:NextFunction) => {
+   
+}
